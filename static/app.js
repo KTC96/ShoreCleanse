f65cfc6ab24e248f5628c3fb81bc2b033ec86a3a
KTC96 <kyleclow96@gmail.com>
2023-09-07 15:40:53 +0000

    Changed size of each section on index and improved animations

--- static/app.js
+++ static/app.js
@@ -3,6 +3,8 @@ const observer = new IntersectionObserver((entries) => {
     console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
     }
   });
 });
