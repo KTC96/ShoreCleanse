79cd58cc1cf5286e6923f82c5fb7e1d56f155e63
rz <rinalds98@gmail.com>
2023-09-08 22:34:13 +0100

    Added blog structure and models, styled all posts page

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.all_posts, name='all_posts'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    
 ]
