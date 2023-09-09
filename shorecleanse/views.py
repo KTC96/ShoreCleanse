cfaeb9d68ff2eee526a145592d68f60f9ae53bec
rz <rinalds98@gmail.com>
2023-09-09 10:05:02 +0100

    connect events to homepage

--- shorecleanse/views.py
+++ shorecleanse/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from Event.models import Event
 
 def index(request):
-    return render(request, 'index.html')
+    eventList = Event.objects.all()
+    return render(request, 'index.html', {'eventList': eventList})
 
 def about(request):
 	return render(request, 'about.html')
